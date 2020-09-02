ff2b38d411736382944b5f95d145ddc551066c45
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-09-01 17:16:23 -0700

    Add 'local' command to list in src/commands/index.js

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,7 +2,7 @@
  * @description Load all of the command modules and add them into the given commander program
  */
 module.exports = (program) => {
-  ["auth", "bash"].forEach((cmd) => {
+  ["auth", "bash", "local"].forEach((cmd) => {
     program.addCommand(require("./" + cmd));
   });
 };
