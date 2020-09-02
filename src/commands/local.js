b26ced43f40122fa83a27171152504f24a5cf0c8
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-09-01 17:15:57 -0700

    Write local command

--- src/commands/local.js
+++ src/commands/local.js
@@ -1,11 +1,22 @@
 const { createCommand } = require("commander");
+const chalk = require("chalk");
 
 const logs = require("../logs");
 const config = require("../config");
-const { getRepl } = require("../utils");
+const { parseRepl } = require("../utils");
 
 async function main(passedRepl) {
-  const replId = await getRepl();
+  // TODO: if passedRepl == "REMOVE", remove it
+  const dir = process.cwd(); // TODO: option for directory, defaulting to cwd
+  const replId = await parseRepl(passedRepl);
+  let { localDirs } = config.getConfig();
+  localDirs = { ...localDirs, [dir]: replId };
+  config.update({ localDirs });
+  console.log(
+    chalk.green(
+      `The repl for ${dir} has been set in config file ${config.getConfigFile()}`
+    )
+  );
 }
 
 module.exports = createCommand()
