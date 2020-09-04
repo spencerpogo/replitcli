976baf1cdf8b205d80fba2bf2f8475d503e819d2
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-09-03 18:37:42 -0700

    Run can output

--- src/commands/run.js
+++ src/commands/run.js
@@ -2,13 +2,21 @@ const { createCommand } = require("commander");
 const open = require("open");
 
 const logs = require("../logs");
-const config = require("../config");
 const { getRepl } = require("../utils");
 const { getClient } = require("../connect");
 
 async function main(passedRepl) {
   const replId = getRepl(passedRepl);
   const conn = await getClient(replId);
+  // TODO: some way to determine whether to use interp2/run2
+  const chan = conn.channel("shellrun2");
+  logs.debug("Running...");
+  await conn.run();
+  chan.on("command", (data) => {
+    if (data.output) {
+      process.stdout.write(data.output);
+    }
+  });
 }
 
 module.exports = createCommand()
