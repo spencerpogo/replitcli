b6440f147162979f2f1801de45057266346e1a95
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-15 17:10:23 -0800

    Initial attempt at environment variables (broken)

--- src/commands/exec.js
+++ src/commands/exec.js
@@ -1,8 +1,9 @@
 const { createCommand } = require("commander");
 const { getRepl } = require("../utils");
 const { getClient } = require("../connect");
+const dotenv = require("dotenv");
 
-const main = async (program, args, { repl }) => {
+const main = async (program, args, { repl, env }) => {
   const replId = await getRepl(repl);
   const conn = await getClient(replId);
 
@@ -12,7 +13,7 @@ const main = async (program, args, { repl }) => {
       process.stdout.write(data.output);
     }
   });
-  await chan.request({ exec: { args: [program, ...args] } });
+  await chan.request({ exec: { args: [program, ...args], env } });
 
   // Required cleanup code
   try {
@@ -21,10 +22,24 @@ const main = async (program, args, { repl }) => {
   process.exit(0);
 };
 
+// Parses value as a key=val environment variable, and returns the object environment
+//  with key set to val. Throws an error through logs.fatal if invalid
+const envParser = (value, environment) => {
+  const parts = dotenv.parse(value);
+  return { ...environment, ...parts };
+};
+
 module.exports = createCommand()
   .name("exec")
   .storeOptionsAsProperties(false)
   .passCommandToAction(false)
   .option("-r, --repl <repl>", "The repl to connect to")
+  .option(
+    "--env <values...>",
+    "A list of environment variables to set, in the form key=val. will be merged " +
+      "with any existing environment, and take precedence.",
+    envParser,
+    {}
+  )
   .arguments("<program> [arguments...]")
   .action(main);
