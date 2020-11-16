76fb5f413dbba60d366452bf96031c6c81cb4ebe
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-16 09:11:05 -0800

    Add dir option to local command

--- src/commands/local.js
+++ src/commands/local.js
@@ -4,13 +4,13 @@ const chalk = require("chalk");
 const config = require("../config");
 const { parseRepl } = require("../utils");
 
-async function main(passedRepl) {
-  // TODO: if passedRepl == "REMOVE", remove it
-  const dir = process.cwd(); // TODO: option for directory, defaulting to cwd
+async function main(passedRepl, { dir }) {
   const replId = await parseRepl(passedRepl);
+
   let { localDirs } = config.getConfig();
   localDirs = { ...localDirs, [dir]: replId };
   config.update({ localDirs });
+
   console.log(
     chalk.green(
       `The repl for ${dir} has been set in config file ${config.getConfigFile()}`
@@ -22,6 +22,11 @@ module.exports = createCommand()
   .storeOptionsAsProperties(false)
   .passCommandToAction(false)
   .name("local")
-  .description("Set the local repl for the current directory")
+  .description("Set the local repl for the a directory")
+  .option(
+    "-d, --dir <directory>",
+    "The directory to set for. Defaults to the current directory",
+    process.cwd()
+  )
   .arguments("<repl>")
   .action(main);
