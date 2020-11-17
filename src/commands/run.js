07454c6555680cb8195afb5efa2d5faf3e2bfd03
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-16 16:05:55 -0800

    Add stop and restart functionality to run

--- src/commands/run.js
+++ src/commands/run.js
@@ -4,25 +4,58 @@ const logs = require("../logs");
 const { getRepl } = require("../utils");
 const { getClient } = require("../connect");
 
-async function main(passedRepl) {
+async function main(passedRepl, { stop, restart }) {
+  const shouldStop = stop || restart;
+  const shouldRun = !stop || restart;
+
+  if (!shouldStop && !shouldRun) {
+    // Don't do anything
+    return;
+  }
+
   const replId = await getRepl(passedRepl);
   const conn = await getClient(replId);
+
   // TODO: some way to determine whether to use interp2/run2
   const chan = conn.channel("shellrun2");
-  logs.debug("Running...");
-  await conn.run();
+
   chan.on("command", (data) => {
     if (data.output) {
       const out = data.output.replace(//gm, ">");
       process.stdout.write(out);
     }
   });
+
+  if (shouldStop) {
+    logs.debug("Stopping...");
+    chan.send({ clear: {} });
+    await new Promise((resolve) => {
+      const res = () => {
+        chan.off("command", onCommand);
+        resolve();
+      };
+
+      const onCommand = (data) => {
+        if (data && data.state === 0) {
+          logs.debug("Resolving");
+          res();
+        }
+      };
+      chan.on("command", onCommand);
+    });
+  }
+  if (shouldRun) {
+    logs.debug("Running...");
+    await conn.run();
+  }
 }
 
 module.exports = createCommand()
   .storeOptionsAsProperties(false)
   .passCommandToAction(false)
   .name("run")
-  .description("Runs the repl.")
+  .description("Runs, stops, or restarts the repl.")
+  .option("--stop", "Stops the repl")
+  .option("--restart", "Restarts the repl")
   .arguments("[repl]")
   .action(main);
